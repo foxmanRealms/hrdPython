fd23324c8b4e48e9f22f2ae93311e806d46cf868
foxmanRealms <nyang42@naver.com>
2022-04-19 17:42:15 +0900

    220419 커밋
    함수 // 모듈

---
+++ Module.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#모듈 생성하기 
+# 모듈이란? 함수, 변수 여러개를 가지고 있는 하나의 파일
+
+
+# In[2]:
+
+
+#더하기 해주는 함수 정의
+def add_num(num1,num2):
+    return num1+num2
+
+
+# In[3]:
+
+
+#빼기해주는 함수 정의
+def sub_num(num1,num2):
+    return num1-num2
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
